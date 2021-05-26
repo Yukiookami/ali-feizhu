4559bc44664accc489ceac00c52ec4d82ad35ad5
1073898183@qq.com <1073898183@qq.com>
2021-05-26 14:36:05 +0900

    项目初始化

--- src/main.js
+++ src/main.js
@@ -1,7 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+// axios
+import axios from './static/js/axios'
+// css resets
+import 'normalize.css'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App)
+
+app.use(store).use(router).mount('#app')
+
+app.config.globalProperties.$http = axios
